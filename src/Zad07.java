f09ef66dc611fc2298285c71dd42e6aa00a6cd17
Daniel Stolarski <d.stolarski@o2.pl>
2017-12-07 13:09:47 +0100

    final commit

---
+++ src/Zad07.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Zad07 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String[] names = new String[5];
+
+        System.out.println("Podaj 5 imion:");
+
+        for (int i = 0; i <= 4; i++) {
+            names[i] = scan.nextLine();
+        }
+
+        System.out.println();
+
+        for (int i = 4; i >= 0; i--) {
+            System.out.println("Cześć " + names[i]);
+        }
+    }
+}
